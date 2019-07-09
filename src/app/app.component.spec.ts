d8fc5316bb6ddfce2712633f7509e3c97c8e851f
System Administrator <root@adminde-mbp-4.client.tw.trendnet.org>
2019-07-09 10:39:39 +0800

    create docker local test env

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,16 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ng-web-app');
   });
 
+  it(`should have as title 'ng-web-app'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('ng-web-app');
+  });
+
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-web-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to new ng-web-app!');
   });
 });
